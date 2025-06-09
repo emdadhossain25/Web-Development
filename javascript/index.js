d2fd421b620a51de09a2b7f8bb04f667ee22212d
emdadhossain25 <emdad.hossain321@gmail.com>
2025-06-10 03:14:32 +0600

    dom object challenge recap

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,19 @@
-var a = 3;
-b = 2;
-console.log(a + b);
+function main() {
+  lifeInWeeks(56);
+}
+function lifeInWeeks(age) {
+  var timeLeftInYears = 90 - age;
+
+  var timeLeftInMonths = timeLeftInYears * 12;
+  var timeLeftInWeeks = timeLeftInMonths * 4;
+  var timeLeftInDays = timeLeftInWeeks * 7;
+  console.log(
+    "You have " +
+      timeLeftInDays +
+      " days left, " +
+      timeLeftInWeeks +
+      " weeks, and " +
+      timeLeftInMonths +
+      " months left"
+  );
+}
