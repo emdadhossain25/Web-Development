ea44e11dd8fa9d25a740570f5ec9f4e63f3b3c30
emdadhossain25 <emdad.hossain321@gmail.com>
2025-06-12 18:57:43 +0600

    patch put post

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -26,10 +26,46 @@ app.get("/filter", (req, res) => {
 });
 
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
 
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(jokes.slice(-1));
+});
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const text = req.body.text;
+  const type = req.body.type;
+  const replacementJoke = {
+    id: id,
+
+    jokeText: text,
+    jokeType: type,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const exisitingJoke = jokes.find((joke) => joke.id === id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || exisitingJoke.jokeText,
+    jokeType: req.body.type || exisitingJoke.jokeType,
+  };
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 
 //7. DELETE Specific joke
 
